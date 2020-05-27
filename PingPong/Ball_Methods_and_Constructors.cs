99ca670164c832afe2d6ab514a4693e64d1b8863
Fkame <nikitos_flame@mail.ru>
2020-05-27 21:54:55 +0300

    initial Ball-class

---
+++ PingPong/Ball_Methods_and_Constructors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong
+{
+    partial class Ball
+    {
+        public Ball() : this(10) { }
+        public Ball(int radius) : this(radius, 10, 10) { }
+        public Ball(int radius, int coordOfCenterX, int coordOfCenterY)
+            : this(radius, coordOfCenterX, coordOfCenterY, System.Drawing.Color.BurlyWood) { }
+        public Ball(int radius, int coordOfCenterX, int coordOfCenterY, System.Drawing.Color color)
+        {
+            this.Radius = radius;
+            this.CoordOfCenterX = coordOfCenterX;
+            this.CoordOfCenterY = coordOfCenterY;
+            this.ColorOfBall = color;
+        }
+
+        /*
+         * Рассчёты базируются на формуле попадания точки в окружность: (x - x0)^2 + (y - y0)^2 <= R^2
+         * где x0, y0 - координаты центра, r - радиус,
+         * x, y - координаты интересующей точки
+         */
+        public bool isTouchBorders(int x, int y)
+        {
+            int x0 = this.CoordOfCenterX;
+            int y0 = this.CoordOfCenterY;
+            int r = this.Radius;
+            return (Math.Pow((x - x0), 2) + Math.Pow((y - y0), 2) <= r * r);
+        }
+     }
+}
---
+++ PingPong/Ball_Properties.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong
+{
+    partial class Ball
+    {
+        private int radius;
+        private int coordOfCenterX;
+        private int coordOfCenterY;
+        public System.Drawing.Color ColorOfBall { get; set; }
+        public int Speed { get; set; }
+
+        public int Radius
+        {
+            get { return this.radius; }
+            set
+            {
+                if (value > 0) this.radius = value;
+                else throw new ArgumentException("No-no-no, not lower that 0");
+            }
+        }
+
+        public int CoordOfCenterX
+        {
+            get { return this.coordOfCenterX; }
+            set
+            {
+                if (value >= 0) this.coordOfCenterX = value;
+                else throw new ArgumentException("No-no-no, not lower that 0");
+            }
+        }
+
+        public int CoordOfCenterY
+        {
+            get { return this.coordOfCenterY; }
+            set
+            {
+                if (value >= 0) this.coordOfCenterY = value;
+                else throw new ArgumentException("No-no-no, not lower that 0");
+            }
+        }
+    }
+}
